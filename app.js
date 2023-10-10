ba289f1640937448d94b9ccacc006b16f1c836b7
Jhosephe Thierry <84161472+jhosephethierry@users.noreply.github.com>
2023-10-10 10:19:03 -0300

    Add files via upload
    teste

---
+++ app.js
@@ -0,0 +1,24 @@
+// variáveis javascript
+
+var name = 'Thierry' // string
+var age = 23 // integer
+var weight = 60.7 // float
+var happy = true // boolean
+var time // undefined
+
+var fruits = ['Banana', 'Uva', 'Abacaxi', 'Goiaba'] // array
+
+var person = {
+    name: 'Thierry',
+    age: 36,
+    weight: 60.7,
+    happy: true
+} // object
+
+console.log(fruits) // imprime array
+console.log(fruits[0]) // imprime indice 0 do array
+
+console.log(person) // imprime object
+console.log(person.name) // imprime variável name do object
+
+console.log(typeof name) // imprime tipo da variável name
