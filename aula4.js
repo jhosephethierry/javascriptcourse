ea30263e9a9241a0ecc0f52d822422b1debbddce
Your Name <you@example.com>
2023-10-13 09:55:01 -0300

    function

---
+++ aula4.js
@@ -0,0 +1,8 @@
+// Operadores de atribuição
+
+var product = 'Camisa'
+var text = 'Temos em estoque '
+
+text += product
+
+console.log(text)
---
+++ aula5.js
@@ -0,0 +1,23 @@
+// Funções
+
+// Função action
+function action() {
+    console.log('Executa uma ação')
+}
+
+action()
+
+// Função soma
+function soma() {
+    return 100
+}
+
+console.log(15 + soma())
+
+// Função sum
+function sum(number1, number2) {
+    return number1 + number2
+}
+
+console.log(sum(10, 50))
+console.log(sum(200,200))
