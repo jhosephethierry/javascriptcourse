938c811a943e6d3f16eaaa7503a5e14f8a4aea22
Your Name <you@example.com>
2023-10-13 10:23:15 -0300

    switch

--- aula6.js
+++ aula6.js
@@ -1,2 +1,67 @@
 // Condicionais
 
+// Define como ativo
+var ativo = true
+var message = ''
+
+if (ativo) {
+    message = 'Está ativo.'
+} 
+else {
+    message = 'Não está ativo.'
+}
+
+console.log(message)
+
+// Define como desativo
+var ativo = false
+var message = ''
+
+if (ativo) {
+    message = 'Está ativo.'
+} 
+else {
+    message = 'Não está ativo.'
+}
+
+console.log(message)
+
+// Semáforo método if else if
+var semaforo = 'Amarelo' // define estado do semáforo
+var message = ''
+
+if (semaforo == 'Verde') {
+    message = 'Pode passar.'
+}
+else if (semaforo == 'Vermelho') {
+    message = 'Pare.'
+}
+else if (semaforo == 'Amarelo') {
+    message = 'Preste atenção.'
+}
+else {
+    message = 'Valor inválido.'
+}
+
+console.log(message)
+
+
+// Semáforo método switch
+var semaforo = 'Verde' // define estado do semáforo
+var message = ''
+
+switch (semaforo) {
+    case 'Verde':
+        message = 'Pode passar.'
+        break
+    case 'Vermelho':
+        message = 'Pare.'
+        break
+    case 'Amarelo':
+        message = 'Atenção.'
+        break
+    default:
+        message = 'Valor inválido.'    
+}
+
+console.log(message)
