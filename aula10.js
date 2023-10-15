ab55738bb641ed56a9c217d99c771e9f2f781357
Your Name <you@example.com>
2023-10-15 10:08:50 -0300

    for while

---
+++ aula10.js
@@ -0,0 +1,26 @@
+// Estrutura de repetição
+
+// for
+for (var count = 0; count <= 10; count ++) {
+    console.log(count)
+}
+
+for (var i = 0; i <= 10; i ++) {
+    console.log(i + 'A')
+}
+
+// while
+var count = 0
+
+while (count <= 100) {
+    console.log(count)
+    count++
+}
+
+// while
+var i = 500
+
+while (i > 5) {
+    console.log(i)
+    i /= 7
+}
---
+++ aula9.js
@@ -0,0 +1,11 @@
+// Operador ternário
+
+var age = 18
+var result = (age >= 18 ? 'Ok' : 'Não ok')
+
+console.log(result)
+
+var age = 16
+var result = (age >= 18 ? 'Ok' : 'Não ok')
+
+console.log(result)
