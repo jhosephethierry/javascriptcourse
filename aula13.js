b7d0c6ac6783927bb48331b945c51778c30ede45
Your Name <you@example.com>
2023-10-18 09:45:11 -0300

    aula 13 e 14

---
+++ aula13.js
@@ -0,0 +1,33 @@
+// Operações em string
+
+var text = 'Hello Cataline'
+var value = text.length
+console.log(value)
+
+var text = 'Hello Cataline'
+var value = text[0]
+console.log(value)
+
+var text = 'Hello Cataline'
+var value = text.replace('o', '0')
+console.log(value)
+
+var text = 'Hello Cataline'
+var value = text.replace('Cataline', 'Google')
+console.log(value)
+
+var text = 'Hello Cataline'
+var value = text.toUpperCase()
+console.log(value)
+
+var text = 'Hello Cataline'
+var value = text.split(' ')
+console.log(value)
+
+var text = 'https//cataline.io'
+var value = text.split('//')
+console.log(value)
+
+var text = 'https//cataline.io'
+var value = text.split('//')
+console.log(value[1])
---
+++ aula14.js
@@ -0,0 +1,17 @@
+// Operações em números
+
+var number = 10.3
+var newValue = number.toString()
+console.log(newValue)
+
+var number = 10.3
+var newValue = Math.trunc(number)
+console.log(newValue)
+
+var number = 10.3
+var newValue = Math.ceil(number)
+console.log(newValue)
+
+var number = 10.3
+var newValue = Math.round(number)
+console.log(newValue)
