997d2509eada31f85c87c866cffc34b553038358
Your Name <you@example.com>
2024-03-01 18:46:45 -0300

    aula3

---
+++ aula3/js/scripts.js
@@ -0,0 +1,19 @@
+console.log("Hello World!");
+
+console.log("Hello New World!");
+
+// declara uma variável
+var a = 1;
+
+a = "Teste";
+
+console.log(a);
+
+// este é um comentário
+
+/* 
+este 
+é 
+um 
+comentário
+*/
