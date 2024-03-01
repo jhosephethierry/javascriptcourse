c09dd535f87194750a2ff4c1188c7686de4a2dc5
Your Name <you@example.com>
2024-03-01 19:06:09 -0300

    aula4

---
+++ aula4/js/scripts.js
@@ -0,0 +1,29 @@
+var teste = 1;
+
+var meuPrimeiroNome = "Jhosephe";
+
+console.log(teste);
+
+teste = "Thierry";
+
+console.log(teste);
+
+console.log(meuPrimeiroNome);
+
+let testando = 1;
+
+const ola = 2;
+
+console.log(testando);
+
+console.log(ola);
+
+var meuNome;
+
+console.log(meuNome);
+
+meuNome = "Jhosephe";
+
+console.log(meuNome);
+
+var meuNome = "Jhosephe";
