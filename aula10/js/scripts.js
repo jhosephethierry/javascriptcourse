7c197d78e2a32c081017ea30df18cf2996b23f37
Your Name <you@example.com>
2024-03-02 10:55:21 -0300

    aula 8,9 e 10

---
+++ aula10/js/scripts.js
@@ -0,0 +1,18 @@
+// Objetos
+var obj = {
+    nome: "Thierry",
+    sobrenome: "Cacau",
+    idade: 36,
+};
+
+console.log(obj);
+console.log(obj.nome);
+console.log(obj.sobrenome);
+console.log(obj.idade);
+
+console.log(typeof obj);
+
+obj.nome = "Jhosephe";
+obj.sobrenome = "Thierry"
+
+console.log("Meu nome é " + obj.nome + " " + obj.sobrenome + " e tenho " + obj.idade + " anos.")
---
+++ aula9/js/scripts.js
@@ -0,0 +1,15 @@
+// hoisting
+console.log(typeof nome);
+
+var nome = null;
+var sobrenome;
+
+console.log(typeof nome);
+console.log(typeof sobrenome);
+
+var nome = "Jhosephe";
+var sobrenome = "Thierry";
+
+console.log(typeof nome);
+console.log(typeof sobrenome);
+console.log(nome + " " + sobrenome);
