7e145161ace6df6cd89565642540e03913476bf6
Your Name <you@example.com>
2024-03-04 19:35:33 -0300

    aula 14

---
+++ aula14/js/scripts.js
@@ -0,0 +1,23 @@
+// else e else if
+var nome = "Jhosephe";
+var idade = 36;
+
+if (nome == "Thierry") {
+    console.log("O nome é Thierry.");
+} 
+else if (nome == "Jhosephe") {
+    console.log("O nome dele é Jhosephe.");
+}
+else if (nome == "Cacau") {
+    console.log("O nome dele é Cacau.");
+}
+else {
+    console.log("Ele possui outro nome.");
+}
+
+if (idade >= 18) {
+    console.log("Entrada liberada.");
+}
+else if (idade < 18) {
+    console.log("Entrada proibida.");
+}
