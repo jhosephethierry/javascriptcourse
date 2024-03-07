8270efad5f3ca238cb7ea41503fa016253fc7431
Your Name <you@example.com>
2024-03-07 09:36:09 -0300

    aula15

---
+++ aula15/js/scripts.js
@@ -0,0 +1,12 @@
+// comparando valor e tipo de dados
+var numero = 4;
+
+if (numero === 4) {
+    console.log("Número é 4 do tipo number");
+}
+
+var numeroS = "4";
+
+if (numeroS === 4) {
+    console.log("Número é 4 do tipo number");
+}
