d03d6afee0a76762b6fda1593c2094c412093e0e
Jhosephe Thierry <jhosephethierry@gmail.com>
2024-07-16 11:43:42 -0300

    com

---
+++ aula18/js/scripts.js
@@ -0,0 +1,38 @@
+// while repetição
+
+var x = 0;
+
+while (x <= 10) {
+    console.log("o valor de x é: " + x);
+    x++;
+}
+
+
+var array = ["jhosephe", "thierry", "Andrade", "Cacau"]
+var y = 0; 
+
+while (y < 4) {
+
+    console.log(array[y]);
+    y++;
+}
+
+
+var word = "thierry"   
+var i = 0;
+
+while (i < 7) {
+
+    console.log(word[i]);
+    i++;
+}
+
+
+var word = "thierry"   
+var i = 0;
+
+while (i < 7) {
+
+    console.log(word[i]);
+    i += 2;
+}
